8c8b92bd4b00e314258957a6bffde1a31f2149f9
Iskello <iskello2017@gmail.com>
2023-06-02 01:27:34 +0300

    dynamic calc non-optimize

--- src/js/modules/calc.js
+++ src/js/modules/calc.js
@@ -1,3 +1,5 @@
+import { getResource } from '../services/requests';
+
 const calc = (size, material, options, promocode, result) => {
 	const sizeBlock = document.querySelector(size),
 		materialBlock = document.querySelector(material),
@@ -7,7 +9,8 @@ const calc = (size, material, options, promocode, result) => {
     
 	let sum = 0;
 
-	const calcFunction = () => {
+	//Статичний калькулятор
+	/* const calcFunction = () => {
 		//формула розрахунку
 		sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
 
@@ -30,8 +33,95 @@ const calc = (size, material, options, promocode, result) => {
 	sizeBlock.addEventListener('change', calcFunction);
 	materialBlock.addEventListener('change', calcFunction);
 	optionsBlock.addEventListener('change', calcFunction);
+	promocodeBlock.addEventListener('input', calcFunction); */
+
+
+
+
+
+
+	//динамічний калькулятор !ВАЖЛИВО! В HTML обов'язково повинен бути атрибут value (без значення) у кожного option
+	const calcFunction = () => {
+		//формула розрахунку
+		sum = Math.round((+sizeBlock.value) * (+materialBlock.value) + (+optionsBlock.value));
+
+		//якщо користувач не ввів нічого в перше або друге поле сповіщаємо користувача про це
+		if (sizeBlock.value == ''  && materialBlock.value == '') {
+			resultBlock.textContent = 'Для расчета нужно выбрать размер и материал картины';
+			//також перевіряємо промокод
+		} else if (sizeBlock.value == '') {
+			resultBlock.textContent = 'Для расчета нужно выбрать размер картины';
+		} else if (materialBlock.value == '') {
+			resultBlock.textContent = 'Для расчета нужно выбрать материал картины';
+		} else if (promocodeBlock.value.trim().toLowerCase() === 'iwantpopart') {
+			resultBlock.textContent = Math.round(sum * 0.7);
+		} else {
+			resultBlock.textContent = sum;
+		}
+	};
+
+	
+
+	sizeBlock.addEventListener('change', () => {
+		Array.from(sizeBlock.children).forEach((item, i) => {			
+			getResource('http://localhost:3000/prices')
+				.then(res => {
+					const sizes = res[0].sizes;
+      
+					if (item.value === sizeBlock.value) {
+						console.log(sizes[i]); // Вивести відповідне значення
+						item.setAttribute('value', sizes[i]);
+						calcFunction();
+					}
+				})
+				.catch(error => console.log(error));
+			
+		});
+	});
+
+
+	materialBlock.addEventListener('change', () => {
+		Array.from(materialBlock.children).forEach((item, i) => {			
+			getResource('http://localhost:3000/prices')
+				.then(res => {
+					const material = res[0].material;
+      
+					if (item.value === materialBlock.value) {
+						console.log(material[i]); // Вивести відповідне значення
+						item.setAttribute('value', material[i]);
+						calcFunction();
+					}
+				})
+				.catch(error => console.log(error));
+			
+		});
+	});
+
+
+	optionsBlock.addEventListener('change', () => {
+		Array.from(optionsBlock.children).forEach((item, i) => {
+			
+			getResource('http://localhost:3000/prices')
+				.then(res => {
+					const option = res[0].option;
+      
+					if (item.value === optionsBlock.value) {
+						console.log(option[i]); // Вивести відповідне значення
+						item.setAttribute('value', option[i]);
+						calcFunction();
+					}
+					
+				})
+				.catch(error => console.log(error));
+			
+		});
+	});
+
 	promocodeBlock.addEventListener('input', calcFunction);
 
+
+
+	
 };
 
 export default calc;
